5d24f2739cd99ca8b262d081d6bd2da4e3f70d5a
Ayala Aguirre Lindolfo Maximiliano <email address>
2021-08-06 11:18:34 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+    console.log('booooooooooooooooop');
   }
   return a + b;
 };
